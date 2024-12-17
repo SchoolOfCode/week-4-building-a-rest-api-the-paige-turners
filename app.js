62b2c4ae35ea7db11ebfb663d234cb101a60e4fb
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2024-12-17 20:03:03 +0000

    Tickets 5 & 6 Completed | Ticket 7 attempted
    Ticket 5 functions with appropriate code, msg and
    displays data. Ticket 6 only displays the appropriate msg.
    Ticket 7 does not successfully run.

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.get("/", function (req, res) {
 //get all of the quotes
 //respond with those quotes
 
+/* --- Ticket 3 --- */
 app.get("/quotes", async function (req, res) {
   const quotes = await getQuotes();
   res.json(quotes);
@@ -37,12 +38,44 @@ app.get("/quotes", async function (req, res) {
 //get the id from the request in express
 //use the id to find the right quote
 //respond with the quote
+
+/* --- Ticket 4 --- */
 app.get("/quotes/:id", async function (req, res) {
   const id = req.params.id;
   const quote = await getQuoteByID(id);
   res.json(quote);
 });
 
+/* --- Ticket 5 --- */
+app.post("/quotes/", async function (req, res){
+  const newQuote = await addQuote(req.body);
+  res.status(201).json({
+    message: 'Resource created successfully',
+    data: newQuote,
+  });
+})
+
+/* --- Ticket 6 --- */
+app.patch("/quotes/:id", async function (req, res){
+  const id = req.params.id;
+  const updateData = req.body;
+  const updatedQuote = await editQuote(id, updateData.quoteText, updateData.author);
+  res.status(200).json({
+    message: 'Resource updated successfully',
+    data: updatedQuote,
+  });
+})
+
+/* --- Ticket 7 --- */
+app.delete("/quotes/:id", async function (req, res){
+  const id = req.params.id;
+  const deleteQuote = await deleteQuote(id);
+  res.status(200).json({
+    message: 'Resource deleted successfully',
+  });
+}
+)
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
+
