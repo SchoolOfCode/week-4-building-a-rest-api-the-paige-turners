120556c78fcfaa631526e7e94137e9cd108c31a8
Jonathan Lee <kutsu89@gmail.com>
2024-12-17 14:41:08 +0000

    tickets 1-4

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
-const app = express();
-const PORT = 3000;
+import { promises as fs } from "node:fs";
 
+import { FILEPATH } from "./config.js";
 import {
   getQuotes,
   getQuoteByID,
@@ -9,11 +9,38 @@ import {
   editQuote,
   deleteQuote,
 } from "./quote.js";
+// import { readQuotes, writeQuotes } from "./helper.js";
+
+const app = express();
+const PORT = 3000;
 
 app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Welcome to the inspirational quotes API");
+});
+
+//listen for a get request
+//the route will be /quotes
+//when we receive a request we want to:
+//get all of the quotes
+//respond with those quotes
+
+app.get("/quotes", async function (req, res) {
+  const quotes = await getQuotes();
+  res.json(quotes);
+});
+
+//listen for get request
+//the route will be /quotes/:id
+//when we receive request we want to:
+//get the id from the request in express
+//use the id to find the right quote
+//respond with the quote
+app.get("/quotes/:id", async function (req, res) {
+  const id = req.params.id;
+  const quote = await getQuoteByID(id);
+  res.json(quote);
 });
 
 app.listen(PORT, function () {
